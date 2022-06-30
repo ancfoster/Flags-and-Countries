acaa1ef22046c6a1bd257eeb06205b7d105b2142
ancfoster <a.foster@outlook.com>
2022-06-30 10:16:51 +0100

    Fix navigation accessibility on how to play page

---
+++ assets/javascript/how_to_play.js
@@ -0,0 +1,6 @@
+/* jshint esversion: 11 */
+
+//When button clicked takes user back to main menu
+document.getElementById("back-button").addEventListener("click", function() {
+    window.location.href='index.html';
+})
